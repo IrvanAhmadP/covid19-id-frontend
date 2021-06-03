f9a868458b3d239b0238513f19502884168243bb
Irvan Ahmad Prasetya <irvan.ap1618@gmail.com>
2021-06-03 16:13:23 +0700

    Remove css

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/nucleo/css/nucleo.min.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
 // custom
